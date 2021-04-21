2b14d38f88a929443b7ca042b8b6ce399cd04759
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-21 20:45:45 -0300

    Criar método de cadastrar Produto

--- src/main/java/br/com/zup/ContatosMarketing/controllers/ProdutoController.java
+++ src/main/java/br/com/zup/ContatosMarketing/controllers/ProdutoController.java
@@ -1,4 +1,21 @@
 package br.com.zup.ContatosMarketing.controllers;
 
+import br.com.zup.ContatosMarketing.models.Produto;
+import br.com.zup.ContatosMarketing.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("produtos/")
 public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+    public Produto registrarProduto (@RequestBody Produto produto){
+      return produtoService.cadastrarProduto(produto);
+  }
 }
--- src/main/java/br/com/zup/ContatosMarketing/services/ProdutoService.java
+++ src/main/java/br/com/zup/ContatosMarketing/services/ProdutoService.java
@@ -1,4 +1,22 @@
 package br.com.zup.ContatosMarketing.services;
 
+import br.com.zup.ContatosMarketing.models.Produto;
+import br.com.zup.ContatosMarketing.repositories.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto cadastrarProduto (Produto produto){
+        try{
+            Produto obj = produtoRepository.save(produto);
+            return produto;
+        }catch (Exception error){
+           throw new  RuntimeException("Produto já cadastrado");
+        }
+    }
 }
