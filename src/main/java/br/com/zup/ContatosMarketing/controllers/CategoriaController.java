0017dd623bcea17de7808c8c387661128e08fb08
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-21 21:40:49 -0300

    Criar método para cadastrar categoria

--- src/main/java/br/com/zup/ContatosMarketing/controllers/CategoriaController.java
+++ src/main/java/br/com/zup/ContatosMarketing/controllers/CategoriaController.java
@@ -1,10 +1,22 @@
 package br.com.zup.ContatosMarketing.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.ContatosMarketing.models.Categoria;
+import br.com.zup.ContatosMarketing.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("categorias/")
 
 public class CategoriaController {
+
+    @Autowired
+    private CategoriaService categoriaService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Categoria registrarCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.cadastrarCategoria(categoria);
+    }
 }
--- src/main/java/br/com/zup/ContatosMarketing/services/CategoriaService.java
+++ src/main/java/br/com/zup/ContatosMarketing/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package br.com.zup.ContatosMarketing.services;
 
+import br.com.zup.ContatosMarketing.models.Categoria;
 import br.com.zup.ContatosMarketing.repositories.CategoriaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,13 @@ public class CategoriaService {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+
+    public Categoria cadastrarCategoria (Categoria categoria){
+        try{
+            Categoria obj = categoriaRepository.save(categoria);
+            return categoria;
+        }catch (Exception error){
+            throw new  RuntimeException("Categoria já cadastrado");
+        }
+    }
 }
