5c5aefb99bf7feee69f6aa29935d3de41ed5d81d
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-04-21 21:33:59 -0300

    Criar CategoriaRepository

---
+++ src/main/java/br/com/zup/ContatosMarketing/repositories/CategoriaRepository.java
@@ -0,0 +1,7 @@
+package br.com.zup.ContatosMarketing.repositories;
+
+import br.com.zup.ContatosMarketing.models.Categoria;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CategoriaRepository extends CrudRepository<Categoria,String> {
+}
